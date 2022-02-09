a01ecd3c6e3c25ae403c50a84d94d0606ddb7e48
Govind Kailathuvalappil Ramakrishnan <gkailathuvalap@vmware.com>
2022-02-09 20:24:41 +0530

    Minor modificatons

--- main.go
+++ main.go
@@ -15,14 +15,24 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
 	greetings := os.Getenv("GREETINGS")
 	if greetings == "" {
 		greetings = randstr.String(16)
 	}
 
+	//function to set the greetings to random if its null
+	// func get_greetings() {
+	// 	if greetings == "" {
+	// 		greetings = randstr.String(16)
+	// 	}
+	// }
+
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello,"+greetings)
+		return c.HTML(http.StatusOK, "Hello "+greetings+" !")
+	})
+
+	e.GET("/random", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, greetings)
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
